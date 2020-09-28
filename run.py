dc5d944ef0c309a9d413c7c2e9ccc9a4ccac207d
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-28 15:40:18 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 import json
-from sys import argv
-try:
-    with open("test.txt", "w") as f:
-        f.write(argv[1])
-    data = json.loads(argv[1])
-    json.dump(data, open("test.json", "w"))
-except:
-    pass
+import os
+
+data = os.getenv("workflow_data")
+json.dump(data,open(f'{data["title"]}.json','w'))
