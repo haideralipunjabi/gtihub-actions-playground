d53ddd890804e878b59e66e7a6421857fd499f7b
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-28 15:04:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import json
 from sys import argv
-print(argv[1])
-data = json.loads(argv[1])
-json.dump(data, open("test.json", "w"))
 with open("test.txt", "w") as f:
     f.write(argv[1])
+data = json.loads(argv[1])
+json.dump(data, open("test.json", "w"))
+
