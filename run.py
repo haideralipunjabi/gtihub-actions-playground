d7cfab89ec7c308d930a8c2aae1e0308c6a50905
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-28 14:32:18 +0530

    test

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-with open("test.txt","w") as f:
-    f.write("TEsT")
+import json
+from sys import argv
+print(argv[1])
+data = json.loads(argv[1])
+json.dump(data, open("test.json", "w"))
+with open("test.txt", "w") as f:
+    f.write(argv[1])
