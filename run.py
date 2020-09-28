771c29be5100fb5670f3f5b0fc21cfe46be03423
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-28 15:52:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-data = os.environ.get("github.event.inputs.workflow_data")
+data = os.environ.get("workflow_data")
 print(data)
 json.dump(data,open(f'{data["title"]}.json','w'))
