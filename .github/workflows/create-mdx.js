29947b361fbd38248144ab96d056430c8a090271
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-29 15:14:59 +0530

    Update create-mdx.js

--- .github/workflows/create-mdx.js
+++ .github/workflows/create-mdx.js
@@ -17,7 +17,7 @@ function download(url, path) {
 }
 
 async function createFile(data) {
-  console.log(data);
+  console.log(data.name);
   let username = data.name.toLowerCase().replace(/ /g, "");
   let downloadPromises = []
   if (Object.keys(data).includes("profilepicture")) {
