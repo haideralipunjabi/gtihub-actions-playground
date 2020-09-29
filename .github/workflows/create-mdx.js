60f1d1e15b4d90daf2c9f2aa8731a592498d2226
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-29 14:55:27 +0530

    test

--- .github/workflows/create-mdx.js
+++ .github/workflows/create-mdx.js
@@ -1,1 +1,76 @@
-console.log("TEST");
+const nunjucks = require("nunjucks");
+const fs = require("fs");
+const { extension } = require("mime-types");
+const axios = require('axios');
+
+function download(url, path) {
+    return axios({
+      url,
+      responseType: 'stream',
+    }).then(response=>
+      new Promise((resolve,reject)=>{
+        const ext = extension(response.headers['content-type'])
+        response.data.pipe(fs.createWriteStream(`${path}.${ext}`))
+        .on('finish', ()=> resolve(ext))
+        .on('error', e=>reject(e));
+      }))
+}
+
+async function createFile(data) {
+  let username = data.name.toLowerCase().replace(/ /g, "");
+  let downloadPromises = []
+  if (Object.keys(data).includes("profilepicture")) {
+    downloadPromises.push(await download(
+      data.profilepicture,
+      `../../public/images/devs/${username}`,
+    ).then(ext=>{
+        data.image_profile_pic = `${username}.${ext}`;
+    }));
+  }
+  if (Object.keys(data).includes("workspacepicture")) {
+    downloadPromises.push(await download(
+      data.workspacepicture,
+      `../../public/images/workspace/${username}`
+    ).then(ext=>{
+      data.image_workspace_pic = `${username}.${ext}`;
+  }));
+  }
+
+  Promise.all(downloadPromises).then(()=>{
+    nunjucks.configure("templates");
+
+    let res = nunjucks.render("developer.njk", { data: data });
+    fs.writeFile(`../../pages/devs/${username}.mdx`, res, (err) => {
+      if (err) return console.log(err);
+    });
+  })
+
+  
+}
+const Questions = [
+  "Who are you, and what do you do?",
+  "What hardware do you use?",
+  "What does your workspace look like?",
+  "Image of your workspace",
+  "What software do you use?",
+  "What programming language would you use if your life depended on it?",
+  "What is your favourite food to have while programming?",
+  "What kind of music do you prefer while working?",
+  "What is the one piece of advice you would give to a developer getting started?",
+  "How can we improve the software development culture in Kashmir?",
+  "One thing that you want to plug about yourself or someone else.",
+];
+
+const data = process.env.workflow_data;
+
+let questions = [];
+Questions.forEach((q, i) => {
+  if (Object.keys(data).includes(`question${i + 1}`)) {
+    questions.push({
+      question: q,
+      response: data[`question${i + 1}`],
+    });
+  }
+});
+data["questions"] = questions;
+createFile(data);
